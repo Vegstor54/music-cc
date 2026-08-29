29f1f82ceeec7a7ee18d0e6e99d52cad91623633
Vegstor54 <volkovtimoha09@gmail.com>
2026-08-30 09:34:50 +1000

    Implement thruster control based on angles
    Added functions to convert angles to signals and set thruster vectors based on yaw and pitch.

--- luap/ax-99.lua
+++ luap/ax-99.lua
@@ -1,5 +1,33 @@
 local sensor = peripheral.wrap("front")
-local TARGET_ID = "1c3d906a-8ee7-4d6c-a850-a40bc091b24a" -- позже заменишь на выбор реальной цели
+local TARGET_ID = "1c3d906a-8ee7-4d6c-a850-a40bc091b24a" -- id мишени
+
+local MAX_ANGLE = math.rad(45) -- подбери под реальный максимум отклонения сопла
+
+local function angleToSignal(angle)
+    local clamped = math.max(-MAX_ANGLE, math.min(MAX_ANGLE, angle))
+    return math.floor((math.abs(clamped) / MAX_ANGLE) * 15)
+end
+
+local function setThrusterVector(yaw, pitch)
+    local yawSignal = angleToSignal(yaw)
+    local pitchSignal = angleToSignal(pitch)
+
+    if yaw > 0 then
+        redstone.setAnalogOutput("right", yawSignal)
+        redstone.setAnalogOutput("left", 0)
+    else
+        redstone.setAnalogOutput("left", yawSignal)
+        redstone.setAnalogOutput("right", 0)
+    end
+
+    if pitch > 0 then
+        redstone.setAnalogOutput("top", pitchSignal)
+        redstone.setAnalogOutput("bottom", 0)
+    else
+        redstone.setAnalogOutput("bottom", pitchSignal)
+        redstone.setAnalogOutput("top", 0)
+    end
+end
 
 while true do
     local res = sensor.scan(true)
